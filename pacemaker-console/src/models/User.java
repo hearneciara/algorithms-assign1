7a12f7322a35177b64e2263932d1738adfad551d
Ciara-Marie <hearneciara@hotmail.com>
2015-10-05 10:40:25 +0100

    TOSTRING-GUAVA

--- pacemaker-console/src/models/User.java
+++ pacemaker-console/src/models/User.java
@@ -1,4 +1,6 @@
 package models;
+import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.MoreObjects.toStringHelper;
 
 public class User 
 {
@@ -17,5 +19,14 @@ public class User
     this.lastName = lastName;
     this.email = email;
     this.password = password;
+  }
+  
+  public String toString()
+  {
+    return toStringHelper(this).addValue(firstName)
+                               .addValue(lastName)
+                               .addValue(password)
+                               .addValue(email)                               
+                               .toString();
   }
 }
