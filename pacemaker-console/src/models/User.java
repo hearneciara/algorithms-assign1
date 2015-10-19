3b534503375ae4e66951d7055ed138f2effb85a7
Ciara-Marie <hearneciara@hotmail.com>
2015-10-19 11:01:48 +0100

    more updated tests

--- pacemaker-console/src/models/User.java
+++ pacemaker-console/src/models/User.java
@@ -27,6 +27,23 @@ public Map<Long, Activity> activities = new HashMap<Long, Activity>();
     this.email = email;
     this.password = password;
   }
+  
+  @Override
+  public boolean equals(final Object obj)
+  {
+    if (obj instanceof User)
+    {
+      final User other = (User) obj;
+      return Objects.equal(firstName, other.firstName) 
+          && Objects.equal(lastName,  other.lastName)
+          && Objects.equal(email,     other.email)
+          && Objects.equal(password,  other.password);
+    }
+    else
+    {
+      return false;
+    }
+  }
 
   
   @Override  
